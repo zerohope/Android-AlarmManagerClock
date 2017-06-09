e366130a01d299b6b0e169ce9626a601fbdf6bb8
zerohopex <a584171811@gmail.com>
2017-06-09 23:55:13 +0800

    4
    放弃Litepal,先用intent凑活一下好了

--- app/src/main/java/com/loonggg/alarmmanager/clock/Main2Activity.java
+++ app/src/main/java/com/loonggg/alarmmanager/clock/Main2Activity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import Model.Times;
 
@@ -15,7 +16,7 @@ public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final TextView textview=(TextView)findViewById(R.id.mess);
+        TextView textview=(TextView)findViewById(R.id.mess);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -30,16 +31,21 @@ public class Main2Activity extends AppCompatActivity {
                 startActivity(intent);
                /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                Times time=new Times();
-                time.find(Times.class,1);
-               if(!time.getTime().equals("")){
-                   textview.setText(time.getDate()+"   "+time.getTime());
-
-               }
 
 
             }
         });
+
+       Times time=new Times();
+
+        time.find(Times.class,0);
+        Toast.makeText(this, "闹钟设置成功"+time.getTime(), Toast.LENGTH_LONG).show();
+       // if(time.getTime().equals("")){
+         // textview.setText("1111");
+      //  }
+
+
+
     }
 
 

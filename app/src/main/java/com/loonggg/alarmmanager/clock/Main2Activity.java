b7764217c54262b3596a518e686771e78192c4c7
zerohopex <a584171811@gmail.com>
2017-06-09 18:26:21 +0800

    3
    3

--- app/src/main/java/com/loonggg/alarmmanager/clock/Main2Activity.java
+++ app/src/main/java/com/loonggg/alarmmanager/clock/Main2Activity.java
@@ -7,11 +7,15 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import Model.Times;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final TextView textview=(TextView)findViewById(R.id.mess);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +30,14 @@ public class Main2Activity extends AppCompatActivity {
                 startActivity(intent);
                /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
+                Times time=new Times();
+                time.find(Times.class,1);
+               if(!time.getTime().equals("")){
+                   textview.setText(time.getDate()+"   "+time.getTime());
+
+               }
+
+
             }
         });
     }

7b7c804b4db4299b294b60f74f6d49304234c683
zerohopex <a584171811@gmail.com>
2017-06-11 17:20:36 +0800

    add firebase
    firebase

---
+++ app/src/main/java/cn/studyjams/s2/sj0121/alarmzero/Main2Activity.java
@@ -0,0 +1,55 @@
+package cn.studyjams.s2.sj0121.alarmzero;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+
+import org.litepal.crud.DataSupport;
+
+import Model.Times;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView textview=(TextView)findViewById(R.id.mess);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+
+            }
+        });
+
+       Times time= DataSupport.findLast(Times.class);
+        if(time!=null) {
+           textview.setText(time.getDate()+" "+time.getTime());
+         DataSupport.deleteAll(Times.class);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+}
